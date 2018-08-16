513c6a72cdf3dec7a151584a471b182353e164ed
maruleba <folaa1234@gmail.com>
2018-08-16 15:44:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,10 +9,22 @@ class App < Sinatra::Base
 
   # This is a sample dynamic route.
   get "/hello/:name" do
+  puts params 
+  
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
   # Code your final two routes here:
-
+  # Create a new route for / goodbye/ :name that returns "Goodbye, (person name)" 
+  
+  
+  get '/ Goodbye' do
+    "Goodbye Fola"
+get '/goodbye/:name' do 
+  put params
+  @user_name = params
+puts params
+end 
+    ""
 end
